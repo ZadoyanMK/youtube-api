ab8d9453a1aa4c4a2e6dcb2870f76b8b4e4c4e05
ZadoyanMK <konzamir@gmail.com>
2019-06-04 19:46:18 +0300

    add saving to db via celery task

--- src/youtube_media/serializers/ThumbnailsSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import Thumbnails
-
-
-class ThumbnailsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Thumbnails
-        fields = '__all__'
