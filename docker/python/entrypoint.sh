51ba7c1e6e5be60c0db3d231c43c628d4f5ef25e
ZadoyanMK <konzamir@gmail.com>
2019-06-17 11:57:45 +0300

    add wait for

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-python manage.py makemigrations 
-python manage.py migrate 
+
+while ! nc -z mysql 3306;
+do
+    echo "waiting for db";
+    sleep 5;
+done;
+while ! nc -z rabbitmq 5672;
+do
+    echo "waiting for rabbitmq";
+    sleep 5;
+done;
+
+echo Connected!;
+
+python manage.py makemigrations
+python manage.py migrate
 echo "Running command '$*'"
 exec su -p - ${PYTHON_RUN_USER} -s /bin/bash -c "$*"
